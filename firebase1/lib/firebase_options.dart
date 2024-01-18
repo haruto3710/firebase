0e2566f3b8f438154e1421d5470722694c439c58
haruto <janpo.308.hm@ezweb.ne.jp>
2024-01-18 15:23:58 +0900

    firebase1

---
+++ firebase1/lib/firebase_options.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for android - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+      //この部分に自身のapiKey~measurementIdをコピー
+      apiKey: " ",
+      authDomain: " ",
+      projectId: " ",
+      storageBucket: " ",
+      messagingSenderId: " ",
+      appId: " ",
+      measurementId: " ");
+}
---
+++ firebase1/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+void main() async {
+  // Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  const app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({super.key});
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Firebase_Analytics')),
+      body: Center(
+          child: Column(children: [
+        Text('$_counter', style: Theme.of(context).textTheme.displayLarge),
+        ElevatedButton(
+          onPressed: () {
+            _incrementCounter();
+            //FirebaseAnalytics
+            FirebaseAnalytics.instance.logEvent(
+              name: ' push button',
+            );
+          },
+          child: Text('add'),
+        ),
+      ])),
+    );
+  }
+}
